2de05df03074b25055653a80c4cd1886f67380b4
luanmachad <luan.machado@prontmed.com>
2022-06-15 08:51:29 -0300

    solving first challenge

---
+++ 01_05_Challenge/Algorithms/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    class Program
+    {
+
+        static String ReverseEachWord(String input) {
+           if (string.IsNullOrEmpty(input))
+                return input;
+
+           var stringSplited = input.Split(" ");
+           var reversed = "";
+
+           foreach(var word in stringSplited)
+           {
+                if (word == stringSplited.Last())
+                    reversed = reversed + Reverse(word);
+                else
+                    reversed = (reversed + Reverse(word) + " ");
+           }
+           
+            return reversed;
+        }
+
+        static String Reverse(String input) {
+            if (string.IsNullOrEmpty(input)) {
+                return input;
+            }
+
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            return new String(arr);
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(ReverseEachWord("Taylor is a great worker"));
+            Console.WriteLine(ReverseEachWord(null));
+            Console.WriteLine(ReverseEachWord("racer racecar madam"));
+            Console.WriteLine(ReverseEachWord("what can I do today"));
+            Console.WriteLine(ReverseEachWord(" "));
+        }
+    }
+}
